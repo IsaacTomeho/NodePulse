e04617c14ad4aa51780433598320adf1d7fed8e8
Isaac Tomeho <119877312+IsaacTomeho@users.noreply.github.com>
2023-11-27 11:43:32 -0500

    build

---
+++ build/precache-manifest.c7af24bad95ed6d54a4669e8e8a92365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e849e0509f4db36821e2f2c51ab7a71",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa41a35a20f23656c8ea",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b1cc9738530a4cbd627",
+    "url": "/static/js/2.c3c0f9a9.chunk.js"
+  },
+  {
+    "revision": "2b91d056f6b8971145c7",
+    "url": "/static/js/3.d9ff1bcd.chunk.js"
+  },
+  {
+    "revision": "aa41a35a20f23656c8ea",
+    "url": "/static/js/main.70398a80.chunk.js"
+  },
+  {
+    "revision": "252379f3af79be91a971",
+    "url": "/static/js/runtime~main.4acfc295.js"
+  }
+]);
